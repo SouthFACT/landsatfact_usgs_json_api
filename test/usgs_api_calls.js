f25b968a170c7912375278f4bbd26a0b978c8620
Matthew Geiger <mgeiger@unca.edu>
2016-09-22 16:53:39 -0400

    Start tests for download and downloadoptions usgs api request codes

--- test/usgs_api_calls.js
+++ test/usgs_api_calls.js
@@ -15,8 +15,12 @@ var USGS_HELPER = require("../lib/usgs_api/usgs_helpers.js");
 
 //get testjson
 const test_datasetfields_request_json = require("../json/test-datasetfields-request.json")
-
 const test_datasetfields_response_json = require("../json/test-datasetfields-response.json")
+
+const test_download_request_json = require("../json/test-download-request.json")
+
+const test_downloadoptions_request_json = require("../json/test-downloadoptions-request.json")
+const test_downloadoptions_response_json = require("../json/test-downloadoptions-response.json")
 
 //set base URL for axios
 axios.defaults.baseURL = USGS_CONSTANT.USGS_URL;
@@ -32,7 +36,7 @@ var test_api_call = function(apiKey, request_code, body){
   const usgs_request_code = USGS_HELPER.get_usgs_response_code(request_code);
 
   //make call to USGS api and return promise
-  return USGS_HELPER.get_usgsapi_response(usgs_request_code, request_body);
+  return USGS_HELPER.get_usgsapi_response(ugss_request_code, request_body);
 
 }
 
@@ -61,7 +65,7 @@ describe('USGS API TESTS', function() {
   });
 
 
-  describe('USGS datasetfields', function() {
+  describe('USGS request code: datasetfields', function() {
 
     it('should be fullfilled', function(done) {
       api_key.then( apiKey => {
@@ -86,5 +90,59 @@ describe('USGS API TESTS', function() {
 
   });
  
+  describe('USGS request code: download', function() {
+
+    it('should be fullfilled', function(done) {
+      api_key.then( apiKey => {
+        const test_promise = test_api_call(apiKey, 'download', test_download_request_json)
+        test_promise.should.be.fulfilled.and.notify(done);
+      })
+    })
+
+    it('response should be an array', function(done) {
+      api_key.then( apiKey => {
+        const test_promise = test_api_call(apiKey, 'download', test_download_request_json)
+        test_promise.then(function(result){
+          try {
+            expect(result).to.be.an('array')
+            done();
+          } catch(err) {
+            done(err);
+          }
+        }, done);
+      })
+    })
+
+  });
+
+  describe('USGS request code: downloadoptions', function() {
+
+    // Each time we run a download request the response url will have a new iid field in the url,
+    // so we can't check the response against a static url
+
+    it('should be fullfilled', function(done) {
+      api_key.then( apiKey => {
+        const test_promise = test_api_call(apiKey, 'downloadoptions', test_downloadoptions_request_json)
+        test_promise.should.be.fulfilled.and.notify(done);
+      })
+    })
+
+    it('response should be an array', function(done) {
+      api_key.then( apiKey => {
+        const test_promise = test_api_call(apiKey, 'downloadoptions', test_downloadoptions_request_json)
+        test_promise.then(function(result){
+          try {
+            expect(result).to.be.like(test_downloadoptions_response_json)
+            done();
+          } catch(err) {
+            done(err);
+          }
+        }, done);
+      })
+    })
+
+  });
+
+
 
 });

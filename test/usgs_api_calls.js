e2a83542f4c093fd64d31ed4c8c1c6fdd1569e16
Matthew Geiger <mgeiger@unca.edu>
2016-09-29 16:16:27 -0400

    Change unit testing style to chai-as-promised 'return X.should.eventually' pattern; finish tests for basic request codes

--- test/usgs_api_calls.js
+++ test/usgs_api_calls.js
@@ -1,5 +1,4 @@
 var axios = require('axios');
-
 var assert = require('assert');
 var chai = require('chai');
 var expect  = require("chai").expect;
@@ -8,29 +7,39 @@ chai.use(require('chai-fuzzy'));
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
-//get modules
-var USGS_CONSTANT = require("../lib/usgs_api/usgs_constants.js");
-var USGS_FUNCTION = require("../lib/usgs_api/usgs_functions.js");
-var USGS_HELPER = require("../lib/usgs_api/usgs_helpers.js");
-
-//get testjson
-const test_datasetfields_request_json = require("../json/test-datasetfields-request.json")
-
-const test_search_request_json = require("../json/test-search.json")
-
-const test_datasetfields_response_json = require("../json/test-datasetfields-response.json")
-const test_search_response_json = require("../json/test-search-response.json")
-
-const test_download_request_json = require("../json/test-download-request.json")
-const test_download_response_regexp = new RegExp('\Qhttp://dds.cr.usgs.gov/ltaauth//sno18/ops/l1/2014/013/028/LC80130282014100LGN00.tar.gz?id=\E[A-Za-z0-9]*\Q&iid=LC80130282014100LGN00&did=\E[0-9]*\Q&ver=production\E')
-const test_downloadoptions_request_json = require("../json/test-downloadoptions-request.json")
-const test_downloadoptions_response_json = require("../json/test-downloadoptions-response.json")
+const USGS_CONSTANT = require("../lib/usgs_api/usgs_constants.js");
+const USGS_FUNCTION = require("../lib/usgs_api/usgs_functions.js");
+const USGS_HELPER = require("../lib/usgs_api/usgs_helpers.js");
 
 //set base URL for axios
 axios.defaults.baseURL = USGS_CONSTANT.USGS_URL;
 
 //login and get promise for api key
 const api_key_promise = USGS_HELPER.get_api_key();
+
+//get testjson
+const test_datasets_request_json = require('../json/test-datasets-request.json')
+const test_datasets_response_json = require('../json/test-datasets-response.json')
+
+const test_datasetfields_request_json = require("../json/test-datasetfields-request.json")
+const test_datasetfields_response_json = require("../json/test-datasetfields-response.json")
+
+const test_download_request_json = require("../json/test-download-request.json")
+// regular expression
+const test_download_response_regexp = /.*tar.gz\?id=[a-zA-Z0-9]*&iid=LC80130282014100LGN00&did=[0-9]*&ver=production/
+
+const test_downloadoptions_request_json = require("../json/test-downloadoptions-request.json")
+const test_downloadoptions_response_json = require("../json/test-downloadoptions-response.json")
+
+const test_metadata_request_json = require("../json/test-metadata-request.json")
+const test_metadata_response_json = require("../json/test-metadata-response.json")
+
+const test_search_request_json = require("../json/test-search-request.json")
+const test_search_response_json = require("../json/test-search-response.json")
+
+const test_getbulkdownloadproducts_request_json = require("../json/test-getbulkdownloadproducts-request.json")
+const test_getbulkdownloadproducts_response_json = require("../json/test-getbulkdownloadproducts-response.json")
+
 
 const test_api_call = function (request_code, body) {
   return api_key_promise.then(
@@ -62,55 +71,51 @@ describe('USGS API TESTS', function() {
       return test_promise.should.eventually.be.like(test_datasetfields_response_json);
     })
   });
- 
+
   describe("request code: 'download'", function() {
     it('download URL structure is what we expect it to be', function() {
       const test_promise = test_api_call('download', test_download_request_json)
       const test_result = test_promise.then(function (response) {
-        console.log('RESPONSE: ', response, '\n')
-        return test_download_response_regexp.test(response[0])
+        const download_url = response[0]
+        return test_download_response_regexp.test(download_url)
       })
       return test_result.should.eventually.equal(true)
     })
   })
 
-/*
-  describe('USGS request code: downloadoptions', function() {
-
-    // Each time we run a download request the response url will have a new iid field in the url,
-    // so we can't check the response against a static url
-
-    it('should be fullfilled', function(done) {
-      api_key.then( apiKey => {
-        const test_promise = test_api_call(apiKey, 'downloadoptions', test_downloadoptions_request_json)
-      })
+  describe("request code: 'search'", function() {
+    it('response json is what we expect it to be', function() {
+      test_promise = test_api_call('search', test_search_request_json)
+      return test_promise.should.eventually.be.like(test_search_response_json)
     })
   })
 
-  describe('USGS search', function() {
-
-    it('should be fullfilled', function(done) {
-      api_key.then( apiKey => {
-        const test_promise = test_api_call(apiKey, 'search', test_search_request_json)
-        test_promise.should.be.fulfilled.and.notify(done);
-      })
+  describe("request code: 'metadata'", function () {
+    it('response json is what we expect it to be', function () {
+      test_promise = test_api_call('metadata', test_metadata_request_json)
+      return test_promise.should.eventually.be.like(test_metadata_response_json)
     })
+  })
 
-    it('response json should match', function(done) {
-      api_key.then( apiKey => {
-        const test_promise = test_api_call(apiKey, 'search', test_search_request_json)
-        test_promise.then(function(result){
-          try {
-            expect(result).to.be.like(test_search_response_json);
-            done();
-          } catch(err) {
-            done(err);
-          }
-        }, done);
-      })
+  describe("request code: 'downloadoptions'", function () {
+    it('response json is what we expect it to be', function () {
+      test_promise = test_api_call('downloadoptions', test_downloadoptions_request_json)
+      return test_promise.should.eventually.be.like(test_downloadoptions_response_json)
     })
+  })
 
-  });
-*/
+  describe("request code: 'datasets'", function () {
+    it('response json is what we expect it to be', function () {
+      test_promise = test_api_call('datasets', test_datasets_request_json)
+      return test_promise.should.eventually.be.like(test_datasets_response_json)
+    })
+  })
+
+  describe("request code: 'getbulkdownloadproducts'", function () {
+    it('response json is what we expect it to be', function () {
+      test_promise = test_api_call('getbulkdownloadproducts', test_getbulkdownloadproducts_request_json)
+      return test_promise.should.eventually.be.like(test_getbulkdownloadproducts_response_json)
+    })
+  })
 
 });

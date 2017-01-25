fbb94b0235f9c697319925452f6c90718abdf082
Matthew Geiger <mgeiger@unca.edu>
2017-01-25 15:46:08 -0500

    refactor work

--- test/unit/test_update_landsat_metadata.js
+++ test/unit/test_update_landsat_metadata.js
@@ -7,7 +7,8 @@ chai.use(require('chai-fuzzy'))
 var chaiAsPromised = require("chai-as-promised")
 chai.use(chaiAsPromised)
 var fs = require('fs')
-
+var Promise = require('bluebird')
+Promise.longStackTraces()
 
 var dataset_fields = require(
   '../json/test_real_usgs_api/test_datasetfields_request.json'
@@ -20,7 +21,7 @@ const update_metadata = require('../../update_landsat_metadata.js')
 const update_lsf_database = require("../../lib/postgres/update_lsf_database.js")
 
 const sample_metadata_xml = fs.readFileSync(
-  './test/sample-datasetfields-get.xml', 'utf8'
+  './test/metadata_xml/LANDSAT_7_LE70330372017016EDC00.xml', 'utf8'
 )
 const sample_metadata = { 'data': sample_metadata_xml }
 var parse_xml = update_metadata.parse_scene_metadata_xml(sample_metadata)

fa54420250612f996bf6f0d75e314f04edca16f2
Dave Michelson <daveism@gmail.com>
2017-05-25 16:01:54 -0400

    remove 'update_landsat_metadata.js' from blocking  api calls this is mostly doing database stuff don't want it to block

--- processes.sh
+++ processes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # declare an array called usgs_api_processes and add any process that uses the api
-#   this should help stop any possible simaltaneous api calls 
-usgs_api_processes=( 'landsatfact-data-dev.nemac.org/project/lsf_metadata_cron.sh' 'landsatFACT_LCV.py' 'update_landsat_metadata.js' 'update_scenes_to_order.js' 'download_landsat_data.php' 'customRequest.py' 'order_landsat_data.js')
-
+#   this should help stop any possible simaltaneous api calls
+usgs_api_processes=( 'landsatfact-data-dev.nemac.org/project/lsf_metadata_cron.sh' 'landsatFACT_LCV.py' 'update_scenes_to_order.js' 'download_landsat_data.php' 'customRequest.py' 'order_landsat_data.js')
